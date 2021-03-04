c831479fe3aec35130445d1505401d878ce153d3
Fernando Britto <fernandobritto.web@gmail.com>
2021-03-04 15:10:21 -0300

    Initial commit

---
+++ 04 - Construtor/index.ts
@@ -0,0 +1,19 @@
+class TypeScript{
+
+    constructor(){
+        console.log('Metodo construtor')
+        this.metodo01()
+        this.metodo02()
+    }
+
+    metodo01(): void {
+        console.log('Metodo 01')
+    }
+
+    metodo02(): void{
+        console.log('Metodo 02')
+    }
+}
+
+const poo = new TypeScript
+
